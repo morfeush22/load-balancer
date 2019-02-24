6a4173582db29da5e598f2bd940bd0b399037919
morfeush22 <patro.kacper@gmail.com>
2019-02-24 23:20:39 +0100

    linking example boost lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/option.hpp>
+using namespace std;
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "produce help message")
+            ;
+
     return 0;
 }
