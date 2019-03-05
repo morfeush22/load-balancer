8410649418698fcf1ab47d9806284ce0c18be3aa
morfeush22 <patro.kacper@gmail.com>
2019-03-05 20:33:18 +0100

    scaffold implementation of LoadBalancer main class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "include/HealthChecker.h"
 #include "include/HealthCheckerFactory.h"
 #include "include/ServersRepository.h"
+#include "include/ProxyConnectionFactory.h"
+#include "include/LoadBalancer.h"
 #include <memory>
 
 
@@ -27,7 +29,9 @@ int main(int argc, char **argv) {
     boost::asio::io_context io_context;
 
     auto health_checker_factory = std::make_unique<HealthCheckerFactory>(io_context, config_parser);
-    auto servers_repository = std::make_unique<ServersRepository>(std::move(health_checker_factory), config_parser);
+    auto servers_repository = std::make_shared<ServersRepository>(std::move(health_checker_factory), config_parser);
+    auto proxy_connection_factory = std::make_unique<ProxyConnectionFactory>(io_context, config_parser, servers_repository);
+    auto load_balancer = std::make_shared<LoadBalancer>(io_context, config_parser, move(proxy_connection_factory));
 
     auto deadline_timer = boost::asio::deadline_timer(io_context, boost::posix_time::seconds(1));
     std::function<void(const boost::system::error_code&)> lambda;
