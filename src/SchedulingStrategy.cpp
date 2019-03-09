17d7da2383b2347fd97df21ae462b7ceab21a842
morfeush22 <patro.kacper@gmail.com>
2019-03-09 20:46:16 +0100

    cleanup of SchedulingStrategy

--- src/SchedulingStrategy.cpp
+++ src/SchedulingStrategy.cpp
@@ -4,14 +4,15 @@
 
 #include "../include/SchedulingStrategy.h"
 
-SchedulingStrategy::SchedulingStrategy(std::unique_ptr<SchedulingAlgorithm> &&scheduling_algorithm):
-scheduling_algorithm_(move(scheduling_algorithm))
-{}
+
+SchedulingStrategy::SchedulingStrategy(std::unique_ptr<SchedulingAlgorithm> &&scheduling_algorithm) :
+        scheduling_algorithm_(move(scheduling_algorithm)) {}
 
 // list can not be empty
 const BackendServerDescription &
 SchedulingStrategy::SelectBackendServer(boost::beast::http::request<boost::beast::http::string_body> frontend_headers,
                                         const std::list<BackendServerDescription> &backend_server_description) {
+    //TODO implementation
     //if has cookie
     //check list for server by id
     //if server found, use it
